d453fbd0a596b8f4ec366fe212abd3c0f564552c
Gerardo2006 <164081228+Gerardo2006@users.noreply.github.com>
2025-11-09 20:32:14 -0600

    feat: Agregando infraestructura inicial del backend

---
+++ BE/Keys/keys.js
@@ -0,0 +1,8 @@
+// Puerto para el backend
+export const PORT = 3000; 
+
+// Secreto para firmar los JSON Web Tokens (JWT)
+export const JWT_SECRET = "uca_game_store_secret_key"; 
+
+// Complejidad para el hash de contraseñas
+export const HASH_COMPLEXITY = 10;
---
+++ BE/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { PORT } from "./Keys/keys.js";
+
+// Importaremos nuestras rutas
+// import apiRouter from "./router/router.js";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+// Ruta de bienvenida para probar que el servidor funciona
+app.get("/", (req, res) => {
+  res.status(200).json({ status: true, message: "¡Bienvenido al Backend de UCA Game Store!" });
+});
+
+
+// --- Iniciar Servidor ---
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+);
